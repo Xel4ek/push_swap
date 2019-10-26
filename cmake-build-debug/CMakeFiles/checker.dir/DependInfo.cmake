39412ce6166d9bfcdb5df0c506ef3551b17395f4
Horse Wolf <hwolf@at-o2.21-school.ru>
2019-10-26 20:54:48 +0300

    done

--- cmake-build-debug/CMakeFiles/checker.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/checker.dir/DependInfo.cmake
@@ -4,20 +4,20 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "/home/alexey/projects/push_swap/checker.c" "/home/alexey/projects/push_swap/cmake-build-debug/CMakeFiles/checker.dir/checker.c.o"
-  "/home/alexey/projects/push_swap/src/dfs.c" "/home/alexey/projects/push_swap/cmake-build-debug/CMakeFiles/checker.dir/src/dfs.c.o"
-  "/home/alexey/projects/push_swap/src/ft_is_str_in_lstd.c" "/home/alexey/projects/push_swap/cmake-build-debug/CMakeFiles/checker.dir/src/ft_is_str_in_lstd.c.o"
-  "/home/alexey/projects/push_swap/src/ft_lstd_swap.c" "/home/alexey/projects/push_swap/cmake-build-debug/CMakeFiles/checker.dir/src/ft_lstd_swap.c.o"
-  "/home/alexey/projects/push_swap/src/ft_lstd_to_str.c" "/home/alexey/projects/push_swap/cmake-build-debug/CMakeFiles/checker.dir/src/ft_lstd_to_str.c.o"
-  "/home/alexey/projects/push_swap/src/ft_lstprint.c" "/home/alexey/projects/push_swap/cmake-build-debug/CMakeFiles/checker.dir/src/ft_lstprint.c.o"
-  "/home/alexey/projects/push_swap/src/ft_operation.c" "/home/alexey/projects/push_swap/cmake-build-debug/CMakeFiles/checker.dir/src/ft_operation.c.o"
-  "/home/alexey/projects/push_swap/src/ft_quick_sort.c" "/home/alexey/projects/push_swap/cmake-build-debug/CMakeFiles/checker.dir/src/ft_quick_sort.c.o"
-  "/home/alexey/projects/push_swap/src/ft_sort_lstd.c" "/home/alexey/projects/push_swap/cmake-build-debug/CMakeFiles/checker.dir/src/ft_sort_lstd.c.o"
-  "/home/alexey/projects/push_swap/src/ft_swap_stacks.c" "/home/alexey/projects/push_swap/cmake-build-debug/CMakeFiles/checker.dir/src/ft_swap_stacks.c.o"
-  "/home/alexey/projects/push_swap/src/is_sorted.c" "/home/alexey/projects/push_swap/cmake-build-debug/CMakeFiles/checker.dir/src/is_sorted.c.o"
-  "/home/alexey/projects/push_swap/src/read.c" "/home/alexey/projects/push_swap/cmake-build-debug/CMakeFiles/checker.dir/src/read.c.o"
+  "/Users/hwolf/projects/push_swap/checker.c" "/Users/hwolf/projects/push_swap/cmake-build-debug/CMakeFiles/checker.dir/checker.c.o"
+  "/Users/hwolf/projects/push_swap/src/dfs.c" "/Users/hwolf/projects/push_swap/cmake-build-debug/CMakeFiles/checker.dir/src/dfs.c.o"
+  "/Users/hwolf/projects/push_swap/src/ft_is_str_in_lstd.c" "/Users/hwolf/projects/push_swap/cmake-build-debug/CMakeFiles/checker.dir/src/ft_is_str_in_lstd.c.o"
+  "/Users/hwolf/projects/push_swap/src/ft_lstd_swap.c" "/Users/hwolf/projects/push_swap/cmake-build-debug/CMakeFiles/checker.dir/src/ft_lstd_swap.c.o"
+  "/Users/hwolf/projects/push_swap/src/ft_lstd_to_str.c" "/Users/hwolf/projects/push_swap/cmake-build-debug/CMakeFiles/checker.dir/src/ft_lstd_to_str.c.o"
+  "/Users/hwolf/projects/push_swap/src/ft_lstprint.c" "/Users/hwolf/projects/push_swap/cmake-build-debug/CMakeFiles/checker.dir/src/ft_lstprint.c.o"
+  "/Users/hwolf/projects/push_swap/src/ft_operation.c" "/Users/hwolf/projects/push_swap/cmake-build-debug/CMakeFiles/checker.dir/src/ft_operation.c.o"
+  "/Users/hwolf/projects/push_swap/src/ft_quick_sort.c" "/Users/hwolf/projects/push_swap/cmake-build-debug/CMakeFiles/checker.dir/src/ft_quick_sort.c.o"
+  "/Users/hwolf/projects/push_swap/src/ft_sort_lstd.c" "/Users/hwolf/projects/push_swap/cmake-build-debug/CMakeFiles/checker.dir/src/ft_sort_lstd.c.o"
+  "/Users/hwolf/projects/push_swap/src/ft_swap_stacks.c" "/Users/hwolf/projects/push_swap/cmake-build-debug/CMakeFiles/checker.dir/src/ft_swap_stacks.c.o"
+  "/Users/hwolf/projects/push_swap/src/is_sorted.c" "/Users/hwolf/projects/push_swap/cmake-build-debug/CMakeFiles/checker.dir/src/is_sorted.c.o"
+  "/Users/hwolf/projects/push_swap/src/read.c" "/Users/hwolf/projects/push_swap/cmake-build-debug/CMakeFiles/checker.dir/src/read.c.o"
   )
-set(CMAKE_C_COMPILER_ID "GNU")
+set(CMAKE_C_COMPILER_ID "AppleClang")
 
 # The include file search paths:
 set(CMAKE_C_TARGET_INCLUDE_PATH
@@ -28,7 +28,7 @@ set(CMAKE_C_TARGET_INCLUDE_PATH
 
 # Targets to which this target links.
 set(CMAKE_TARGET_LINKED_INFO_FILES
-  "/home/alexey/projects/push_swap/cmake-build-debug/lib/libft/CMakeFiles/libft.dir/DependInfo.cmake"
+  "/Users/hwolf/projects/push_swap/cmake-build-debug/lib/libft/CMakeFiles/libft.dir/DependInfo.cmake"
   )
 
 # Fortran module output directory.
--- lib/libft/src/ft_strlen.c
+++ lib/libft/src/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: hwolf <hwolf@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/12 13:03:30 by hwolf             #+#    #+#             */
-/*   Updated: 2019/10/19 19:27:47 by hwolf            ###   ########.fr       */
+/*   Updated: 2019/10/26 17:35:36 by hwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,55 @@
 
 size_t	ft_strlen(const char *str)
 {
-	size_t len;
+	const char *char_ptr;
+	const unsigned long int *longword_ptr;
+	unsigned long int longword;
+	unsigned long int himagic;
+	unsigned long int lomagic;
 
-	len = 0;
-	if (str)
-		while (*str++)
-			++len;
-	return (len);
+	char_ptr = str;
+	while ((unsigned long int) char_ptr & (sizeof (longword) - 1))
+	{
+		 ++char_ptr;
+		if (*char_ptr == '\0')
+			return char_ptr - str;
+	}
+
+	longword_ptr = (unsigned long int *) char_ptr;
+	himagic = 0x80808080L;
+	lomagic = 0x01010101L;
+	if (sizeof (longword) > 4)
+	{
+		himagic = ((himagic << 16) << 16) | himagic;
+		lomagic = ((lomagic << 16) << 16) | lomagic;
+	}
+
+	while (1)
+	{
+		longword = *longword_ptr++;
+		if (((longword - lomagic) & ~longword & himagic) != 0)
+		{
+			const char *cp = (const char *) (longword_ptr - 1);
+			if (cp[0] == 0)
+				return cp - str;
+			if (cp[1] == 0)
+				return cp - str + 1;
+			if (cp[2] == 0)
+				return cp - str + 2;
+			if (cp[3] == 0)
+				return cp - str + 3;
+			if (sizeof (longword) > 4)
+			{
+				if (cp[4] == 0)
+					return cp - str + 4;
+				if (cp[5] == 0)
+					return cp - str + 5;
+				if (cp[6] == 0)
+					return cp - str + 6;
+				if (cp[7] == 0)
+					return cp - str + 7;
+			}
+		}
+	}
+
 }
